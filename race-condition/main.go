9fed80852b347469bea5d7b099e7d7f6daf3188e
Dzkzzz <dzdzaakk@gmail.com>
2025-03-31 23:47:03 +0700

    learn race condition

---
+++ race-condition/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// race main
+// func main() {
+// 	// This is our shared variable
+// 	counter := 0
+
+// 	// We'll use this to wait for all goroutines to finish
+// 	var wg sync.WaitGroup
+
+// 	// Launch 1000 goroutines
+// 	for i := 0; i < 1000; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			// Each goroutine increments the counter
+// 			counter++
+// 			wg.Done()
+// 		}()
+// 	}
+
+// 	// Wait for all goroutines to complete
+// 	wg.Wait()
+
+// 	// Print the final value
+// 	fmt.Println("Final counter value:", counter)
+// }
+
+// fixed with mutex
+// func main() {
+// 	// This is our shared variable
+// 	counter := 0
+
+// 	// We'll use this to wait for all goroutines to finish
+// 	var wg sync.WaitGroup
+
+// 	// This mutex will protect our counter
+// 	var mu sync.Mutex
+
+// 	// Launch 1000 goroutines
+// 	for i := 0; i < 1000; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			// Lock the mutex before accessing the counter
+// 			mu.Lock()
+// 			counter++
+// 			// Unlock when done
+// 			mu.Unlock()
+
+// 			wg.Done()
+// 		}()
+// 	}
+
+// 	// Wait for all goroutines to complete
+// 	wg.Wait()
+
+// 	// Print the final value
+// 	fmt.Println("Final counter value:", counter)
+// }
+
+// fixed with atomic operations
+func main() {
+	// Use an atomic counter
+	var counter int64 = 0
+
+	// We'll use this to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Launch 1000 goroutines
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			// Use atomic operation to increment
+			atomic.AddInt64(&counter, 1)
+			wg.Done()
+		}()
+	}
+
+	// Wait for all goroutines to complete
+	wg.Wait()
+
+	// Print the final value
+	fmt.Println("Final counter value:", counter)
+}

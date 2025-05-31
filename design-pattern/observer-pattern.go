10b1f44e220328bf091392943c6b8c9c0c522fda
Dzkzzz <dzdzaakk@gmail.com>
2025-05-31 22:46:57 +0700

    observer pattern

---
+++ design-pattern/observer-pattern.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Observer interface {
+	Update(data string)
+}
+
+type Subject struct {
+	observers []Observer
+}
+
+func (s *Subject) Register(o Observer) {
+	s.observers = append(s.observers, o)
+}
+
+func (s *Subject) Notify(data string) {
+	for _, o := range s.observers {
+		o.Update(data)
+	}
+}
+
+type Logger struct{}
+
+func (l *Logger) Update(data string) {
+	fmt.Println("Logger received:", data)
+}
+
+// func main() {
+// 	subject := &Subject{}
+// 	logger := &Logger{}
+
+// 	subject.Register(logger)
+// 	subject.Notify("New event occurred")
+// }

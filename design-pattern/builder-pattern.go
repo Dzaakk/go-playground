b118dc8d018ed079da169aeab8279349782947ce
Dzkzzz <dzdzaakk@gmail.com>
2025-06-01 22:20:38 +0700

    builder pattern

---
+++ design-pattern/builder-pattern.go
@@ -0,0 +1,35 @@
+package main
+
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+type UserBuilder struct {
+	user User
+}
+
+func (ub *UserBuilder) SetName(name string) *UserBuilder {
+	ub.user.Name = name
+	return ub
+}
+
+func (ub *UserBuilder) SetEmail(email string) *UserBuilder {
+	ub.user.Email = email
+	return ub
+}
+
+func (ub *UserBuilder) SetAge(age int) *UserBuilder {
+	ub.user.Age = age
+	return ub
+}
+
+func (ub *UserBuilder) Build() User {
+	return ub.user
+}
+
+// func main() {
+// 	user := (&UserBuilder{}).SetName("Bob").SetEmail("bob@wmail.com").SetAge(30).Build()
+// 	fmt.Println(user)
+// }

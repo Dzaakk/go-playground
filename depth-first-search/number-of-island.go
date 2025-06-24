33ac06a340de3618643212e16fd4307ec8d69082
Dzkzzz <dzdzaakk@gmail.com>
2025-06-24 21:38:16 +0700

    number of islands

---
+++ depth-first-search/number-of-island.go
@@ -0,0 +1,35 @@
+package main
+
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+	count := 0
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0' {
+			return
+		}
+
+		grid[r][c] = '0'
+
+		dfs(r-1, c)
+		dfs(r+1, c)
+		dfs(r, c-1)
+		dfs(r, c+1)
+	}
+
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			if grid[r][c] == '1' {
+				count++
+				dfs(r, c)
+			}
+		}
+	}
+	return count
+}
